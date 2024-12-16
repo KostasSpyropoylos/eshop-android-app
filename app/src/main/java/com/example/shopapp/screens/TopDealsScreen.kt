ccd2923b2b3951e551bbb3e9cb8f83fd5947fdd4
KSpyropoulos <kspyropoulos@covariance.gr>
2024-12-16 22:00:29 +0200

    added topdealsscreen

--- app/src/main/java/com/example/shopapp/screens/TopDealsScreen.kt
+++ app/src/main/java/com/example/shopapp/screens/TopDealsScreen.kt
@@ -1,6 +1,7 @@
 package com.example.shopapp.screens
 
 import android.util.Log
+import androidx.compose.foundation.layout.Row
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.mutableStateListOf
@@ -8,39 +9,50 @@ import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.navigation.NavHostController
 import com.example.shopapp.data.Product
+import com.example.shopapp.data.ProductDTO
 import com.example.shopapp.screens.shared.DynamicVerticalGrid
+import com.example.shopapp.viewmodels.AuthViewModel
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.firestore.FirebaseFirestore
 
 @Composable
-fun ProductListScreen(
+fun TopDealsScreen(
     modifier: Modifier,
     navController: NavHostController,
-    categoryId: String?
+    authViewModel: AuthViewModel
 ) {
     val db = FirebaseFirestore.getInstance()
 
-    val productList = remember { mutableStateListOf<Product>() }
+    var productList = remember { mutableStateListOf<Product>() }
 
-    LaunchedEffect(categoryId) {
+    LaunchedEffect(Unit) {
         db.collection("products")
-            .whereEqualTo("categoryId", categoryId)
             .get()
             .addOnSuccessListener { result ->
                 productList.clear()
                 for (document in result) {
                     val dataModal: Product = document.toObject(Product::class.java)
-                    productList.add(dataModal)
+                    if(dataModal.discountedPrice>0) {
+                        productList.add(dataModal)
+                    }
                 }
+                productList.sortByDescending { it.discountedPrice }
             }
             .addOnFailureListener { e ->
                 Log.w("Firestore", "Error fetching products", e)
             }
     }
+    var products =  productList.sortedByDescending  { calculateDiscountPercentage(it) }
 
-    DynamicVerticalGrid(modifier, productList,navController)
+    DynamicVerticalGrid(Modifier,products,navController)
 }
 
+fun calculateDiscountPercentage(product: Product): Float {
+    val originalPrice = product.price
+    val discountedPrice = product.discountedPrice
+    val discountPercentage = ((originalPrice - discountedPrice) / originalPrice) * 100
+    return String.format("%.2f", discountPercentage).toFloat()
+}
 
 
 
