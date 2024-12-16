ccf0f67060c7581718f2fefdeff2def3a197b94c
KSpyropoulos <kspyropoulos@covariance.gr>
2024-12-16 22:01:07 +0200

    update product data class

--- app/src/main/java/com/example/shopapp/data/Product.kt
+++ app/src/main/java/com/example/shopapp/data/Product.kt
@@ -3,12 +3,15 @@ package com.example.shopapp.data
 import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.mutableStateOf
 import com.google.firebase.database.IgnoreExtraProperties
+import com.google.firebase.database.PropertyName
+
 @IgnoreExtraProperties
 data class Rating(
     val userId: String = "",
     val rating: Float = 0f,       // Rating value from 1 to 5
     val review: String = ""    // Review text
 )
+
 @IgnoreExtraProperties
 data class Specifications(
     val display: String = "",
@@ -21,16 +24,29 @@ data class Specifications(
     val publisher: String = "",
     val language: String = ""
 )
-@IgnoreExtraProperties
+
+//@IgnoreExtraProperties
 data class Product(
     val name: String = "",
     val imageUrl: String = "",
     val description: String = "",
     val categoryId: String = "",
     val price: Float = 0f,
+    val discountedPrice: Float = 0f,
     val colors: List<String> = emptyList(),
     val specifications: Specifications = Specifications(),  // Specifications object
     val reviews: List<Rating> = emptyList(),  // List of Rating objects
-    val selectedColor: String = "",
-    var quantity: Int = 0
+    var selectedColor: String = "",
+    var quantity: Int = 0,
+    @field:JvmField
+    var isTrending: Boolean = false
+)
+@IgnoreExtraProperties
+data class ProductDTO(
+    val name: String = "",
+    val imageUrl: String = "",
+    val price: Float = 0f,
+    val discountedPrice: Float = 0f,
+    @field:JvmField
+    var isTrending: Boolean = false
 )
